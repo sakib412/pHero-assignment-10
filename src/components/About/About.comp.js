7ce99d06059ebb8e54b0ab5d55729bd3e8bddf15
Najmus Sakib <nazmusakib412@gmail.com>
2022-04-18 23:19:19 +0100

    [ADDED] abu=out

--- src/components/About/About.comp.js
+++ src/components/About/About.comp.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div style={{ minHeight: '70vh', textAlign: 'center', marginTop: '1rem' }}>I want to be a good full stack developer.
+            <br />
+            I am currently doing e job in IT firm as a frontend developer <br />
+            I joined this course specially for community
+        </div>
     )
 }
 
